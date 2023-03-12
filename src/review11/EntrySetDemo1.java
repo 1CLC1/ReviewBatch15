88538345271b2c23cdcad6be230e60a7eacd1ac6
1CLC1 <ChrisC1L1C@gmail.com>
2023-03-12 16:08:18 -0400

    review11

---
+++ src/review11/EntrySetDemo1.java
@@ -0,0 +1,19 @@
+package review11;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EntrySetDemo1 {
+    public static void main(String[] args) {
+
+        Map<String,Double> fruitMap=new HashMap<>();
+        fruitMap.put("Apple",12.5);
+        fruitMap.put("Banana",4.9);
+        fruitMap.put("Dragon Fruit",5.6);
+        fruitMap.put("Pear",16.5);
+        fruitMap.put("Orange",15.5);
+        fruitMap.put("Mango",10.6);
+        // Remove only those elements which contain the letter a or e in their name and their price is more than 8 dollars
+
+    }
+}
